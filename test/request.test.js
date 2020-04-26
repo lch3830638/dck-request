11e55c6df3d75e81a30b55f05ac774f45b8867c4
lch3830638 <814353421@qq.com>
2020-04-26 16:41:58 +0800

    添加测试代码

--- test/request.test.js
+++ test/request.test.js
@@ -1,11 +1,55 @@
 import request from '../index'
 
-// test基本请求
+// test请求成功
 (() => {
   request({ url: 'https://unidemo.dcloud.net.cn/api/news' }).then(res => {
-    console.log('baseRequest：success')
-  }).catch(() => {
-    console.log('baseRequest：fail')
+    const exceptRes = {
+      status: 200,
+      data: {},
+      statusText: 'request:ok',
+      headers: {},
+      config: {},
+    }
+    if (Object.keys(exceptRes).toString() === Object.keys(res).toString()) {
+      console.log('requestSuccess: pass')
+    } else {
+      console.error('requestSuccess: fail')
+    }
+  })
+})();
+
+// test请求失败
+(() => {
+  request({
+    url: 'https://unidemo.dcloud.net.cn/api/news',
+    method: 'post',
+  }).catch(err => {
+    const exceptErr = new Error('Request failed with status code 405')
+    exceptErr.config = {}
+    exceptErr.response = {}
+    const verifyErrMsg = err.toString().indexOf('Request failed with status code') !== -1
+    const verifyErrProps = Object.keys(err).toString() === Object.keys(exceptErr).toString()
+    if (verifyErrMsg && verifyErrProps) {
+      console.log('requestFail: pass')
+    } else {
+      console.log('requestFail: fail')
+    }
+  })
+})();
+
+// test请求超时
+(() => {
+  request({
+    url: 'https://unidemo.dcloud.net.cn/api/news',
+    timeout: 2,
+    method: 'post',
+  }).catch(err => {
+    const verifyErrMsg = err.toString().indexOf('timeout of') !== -1
+    if (verifyErrMsg && err.config !== undefined && err.response === null) {
+      console.log('requestTimeout: pass')
+    } else {
+      console.log('requestTimeout: fail')
+    }
   })
 })();
 
@@ -13,7 +57,7 @@ import request from '../index'
 (() => {
   const request1 = request.create({ baseURL: 'https://unidemo.dcloud.net.cn/api' })
   request1({ url: '/news' }).then(res => {
-    console.log('globalConfig：success')
+    console.log('globalConfig：pass')
   }).catch(() => {
     console.log('globalConfig：fail')
   })
@@ -23,11 +67,11 @@ import request from '../index'
 (() => {
   const request2 = request.create({ baseURL: 'https://unidemo.dcloud.net.cn/api' })
   request2.interceptors.request.use(config => {
-    console.log('requestInterceptors：success')
+    console.log('requestInterceptors：pass')
     return config
   })
   request2.interceptors.response.use(res => {
-    console.log('responseInterceptors：success')
+    console.log('responseInterceptors：pass')
     return res
   })
   request2({ url: '/news' })
@@ -38,7 +82,7 @@ import request from '../index'
   const request3 = request.create({ baseURL: 'https://unidemo.dcloud.net.cn/api' })
   request3.interceptors.request.use(config => {
     if (config.customData.a === 1) {
-      console.log('customData: success')
+      console.log('customData: pass')
     } else {
       console.log('customData: fail')
     }
