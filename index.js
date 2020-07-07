6181f2955ed6eb28676b3c3d9f302df9d1c7297b
lch3830638 <814353421@qq.com>
2020-07-07 18:23:36 +0800

    chore: 入口文件修改

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-  module.exports = require('./lib/lib_dist/dck-request.js')
+  module.exports = require('./libDist/dck-request.js')
 } else {
-  module.exports = require('./lib/lib_dist/dck-request.min.js')
+  module.exports = require('./libDist/dck-request.min.js')
 }
